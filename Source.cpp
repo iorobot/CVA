e9297069f915891117787ad98c9e81b58d9dd68d
Ciofini <gciofini@deloitte.it>
2017-05-22 12:39:07 +0200

    non mi riesce fare amend e quindi ho solo aggiunto il Source

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "Bootstrap.h"
 
 using namespace std;
 
 int main(){
-	int x = 3;
-	cout << "area:" << x; 
+	string file = "MarketData_Crab.csv";
+	Bootstrap Curve(5, 6, 7);
+	Curve.GetInfo();
+	
+	//cout << "Numero Depo" << Data << endl;
+	//cout << "Numero Fra" << Data[1] << endl;
+	//cout << "Numero Swap" << Data[2] << endl;
 	getchar();
 	return 0;
 }
